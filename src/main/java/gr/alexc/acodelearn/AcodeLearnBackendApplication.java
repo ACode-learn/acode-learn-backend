dafe4844265e38b979094b8a3a920d100c54cb82
alexc <alexchih02@gmail.com>
2024-11-27 21:51:37 +0200

    initial commit

---
+++ src/main/java/gr/alexc/acodelearn/AcodeLearnBackendApplication.java
@@ -0,0 +1,13 @@
+package gr.alexc.acodelearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcodeLearnBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AcodeLearnBackendApplication.class, args);
+    }
+
+}
