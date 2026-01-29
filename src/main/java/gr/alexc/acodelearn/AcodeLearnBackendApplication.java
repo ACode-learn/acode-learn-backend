cba6a2cd092475596c71ca7a8691a741926abfc6
alexandros tsichouridis <alexchih02@gmail.com>
2026-01-29 21:07:59 +0200

    Changed to spring boot 4

--- src/main/java/gr/alexc/acodelearn/AcodeLearnBackendApplication.java
+++ src/main/java/gr/alexc/acodelearn/AcodeLearnBackendApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AcodeLearnBackendApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(AcodeLearnBackendApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(AcodeLearnBackendApplication.class, args);
+	}
 
 }
